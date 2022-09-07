778f3324c8b70ff6a006c0b5e7cff14ae7ce70f5
henchicky <henchicky@gmail.com>
2022-09-08 01:07:45 +0800

    initial frontend components

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,10 +1,47 @@
+import { Button, Card, TextField } from "@mui/material";
+import { Box, Grid } from "@mui/system";
+import axios from "axios";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [url, setUrl] = useState(0);
+  const [url, setUrl] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
-  return <div className="App"></div>;
+  function handleChange(e) {
+    setUrl(e.target.value);
+  }
+
+  function submit() {
+    setDisabled(true);
+    setUrl("www.short.com");
+    // axios.post("");
+  }
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={3}>
+        <Box m="auto">
+          <Card>
+            <TextField
+              placeholder="Enter your url here"
+              value={url || ""}
+              onChange={handleChange}
+              disabled={disabled}
+            />
+            <Button onClick={submit}>Shorten URl</Button>
+          </Card>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
