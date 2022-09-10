d106b7e44ae3361a8dd805ea6668bd47c02b42e5
TITANSOFT\hendrick.ling99 <henchicky@gmail.com>
2022-09-11 00:00:19 +0800

    add material icons

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,4 +1,16 @@
-import { Button, Card, TextField, Grid } from "@mui/material";
+import {
+  Button,
+  Card,
+  TextField,
+  Grid,
+  CardContent,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { Box } from "@mui/system";
 import axios from "axios";
 import { useState } from "react";
@@ -18,6 +30,13 @@ function App() {
     // axios.post("");
   }
 
+  function copy() {}
+
+  function newUrl() {
+    setDisabled(false);
+    setUrl(null);
+  }
+
   return (
     <Grid
       container
@@ -30,13 +49,46 @@ function App() {
       <Grid item xs={3}>
         <Box m="auto">
           <Card>
-            <TextField
-              placeholder="Enter your url here"
-              value={url || ""}
-              onChange={handleChange}
-              disabled={disabled}
-            />
-            <Button onClick={submit}>Shorten URl</Button>
+            <CardContent>
+              <TextField
+                placeholder="Enter your url here"
+                value={url || ""}
+                onChange={handleChange}
+                disabled={disabled}
+              />
+              <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                  Password
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? "text" : "password"}
+                  value={values.password}
+                  onChange={handleChange("password")}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        <ContentCopyIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              {!disabled ? (
+                <Button onClick={submit}>Shorten URl</Button>
+              ) : (
+                <>
+                  <Button onClick={copy}>Copy</Button>
+                  <Button onClick={newUrl}>Shorten another url</Button>
+                </>
+              )}
+            </CardContent>
           </Card>
         </Box>
       </Grid>
