26d294629526b6d5fc2c456e278c1ab4e1f091e2
TITANSOFT\hendrick.ling99 <henchicky@gmail.com>
2022-09-11 13:24:42 +0800

    add react toastify

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
@@ -21,7 +23,7 @@ function App() {
   }
 
   function shortenUrl() {
-    if (!isValidUrl(url)) alert("Invalid url");
+    if (!isValidUrl(url)) toast.error("Invalid URL");
 
     setDisabled(true);
     setUrl("www.short.com");
@@ -45,51 +47,60 @@ function App() {
   }
 
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item xs={5}>
-        <Box mx="auto">
-          <Card>
-            <CardContent>
-              <Typography align="center" variant="h4">
-                Shorten Now
-              </Typography>
-              <FormControl disabled={disabled}>
-                <OutlinedInput
-                  value={url}
-                  onChange={handleChange}
-                  endAdornment={
-                    disabled && <Button variant="outlined">Copy</Button>
-                  }
-                  placeholder="Enter your url here"
-                  sx={{ my: 2 }}
-                />
-                {!disabled ? (
-                  <Button onClick={shortenUrl} variant="contained">
-                    Shorten URl
-                  </Button>
-                ) : (
-                  <>
-                    <Button onClick={copy} variant="outlined">
-                      Copy
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={5}>
+          <Box mx="auto">
+            <Card>
+              <CardContent>
+                <Typography align="center" variant="h4">
+                  Shorten Now
+                </Typography>
+                <FormControl disabled={disabled}>
+                  <OutlinedInput
+                    value={url}
+                    onChange={handleChange}
+                    endAdornment={
+                      disabled && <Button variant="outlined">Copy</Button>
+                    }
+                    placeholder="Enter your url here"
+                    sx={{ my: 2 }}
+                  />
+                  {!disabled ? (
+                    <Button onClick={shortenUrl} variant="contained">
+                      Shorten URl
                     </Button>
+                  ) : (
                     <Button onClick={newUrl} variant="contained">
                       Shorten another url
                     </Button>
-                  </>
-                )}
-              </FormControl>
-            </CardContent>
-          </Card>
-        </Box>
+                  )}
+                </FormControl>
+              </CardContent>
+            </Card>
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <ToastContainer />
+    </>
   );
 }
 
