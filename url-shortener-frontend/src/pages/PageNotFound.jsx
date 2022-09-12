e00697ac4b78798d93c1ed8d5e02b9e49bb7b75f
TITANSOFT\hendrick.ling99 <henchicky@gmail.com>
2022-09-13 02:04:03 +0800

    update page not found

--- url-shortener-frontend/src/pages/PageNotFound.jsx
+++ url-shortener-frontend/src/pages/PageNotFound.jsx
@@ -1,4 +1,4 @@
-import { Typography, Container, Link } from "@mui/material";
+import { Typography, Container, Link, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import NavBar from "./NavBar";
 import NotFoundImg from "../asset/404.png";
@@ -15,9 +15,38 @@ function PageNotFound() {
       >
         <Container maxWidth="sm" sx={{ textAlign: "center" }}>
           <img src={NotFoundImg} alt="404-not-found" width="80%" />
-          <Typography variant="h6">Opps! Page not found.</Typography>
-          <Link underline="none" href="/">
-            Click here to go back to main page.
+          <Typography
+            variant="body1"
+            sx={{
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              fontSize: "1.8rem",
+            }}
+          >
+            404 Page Not Found
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              fontFamily: "monospace",
+              fontWeight: 700,
+              color: "inherit",
+              marginTop: "20px",
+            }}
+          >
+            You should probably
+          </Typography>
+          <Link
+            underline="none"
+            href="/"
+            variant="body1"
+            sx={{
+              fontFamily: "monospace",
+              fontWeight: 700,
+            }}
+          >
+            go back to home page.
           </Link>
         </Container>
       </Box>
