3c86a9d0295589b6744a390af43b2005c83611f6
TITANSOFT\hendrick.ling99 <henchicky@gmail.com>
2022-09-13 22:04:43 +0800

    try heroku secret key

--- url-shortener-backend/url-shortener-backend/Program.cs
+++ url-shortener-backend/url-shortener-backend/Program.cs
@@ -3,13 +3,13 @@ using url_shortener_backend.Helpers;
 using url_shortener_backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("UrlShortenerDatabase")));
+    opt.UseNpgsql(GetConnectioinString()));
 builder.Services.AddScoped<IUrlService, UrlService>();
 
 var app = builder.Build();
@@ -33,3 +33,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+string GetConnectioinString()
+{
+    var envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
+    string connectionString;
+    if (string.IsNullOrEmpty(envVar))
+    {
+        connectionString = builder.Configuration.GetConnectionString("UrlShortenerDatabase");
+    }
+    else
+    {
+        var uri = new Uri(envVar);
+        var username = uri.UserInfo.Split(':')[0];
+        var password = uri.UserInfo.Split(':')[1];
+        connectionString =
+            "; Database=" + uri.AbsolutePath.Substring(1) +
+            "; Username=" + username +
+            "; Password=" + password +
+            "; Port=" + uri.Port +
+            "; Host=" + uri.Host +
+            "; SSL Mode=Require; Trust Server Certificate=true;";
+    }
+
+    return connectionString;
+}
